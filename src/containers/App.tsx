21464b54198063a0c9c7ae0d168b39969e4a2f73
Matthew Holland <mtholland10@gmail.com>
2020-08-16 18:03:58 -0400

    Rename navigations dir

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 
-import { Navigation as RootNavigation } from '../navigation';
+import { Navigation as RootNavigation } from '../navigations';
 import { configureStore } from 'redux/store';
 
 const store = configureStore();
 
-const App = () => (
-  <Provider store={store}>
-    <SafeAreaView style={{ flex: 1 }}>
-      <RootNavigation />
-    </SafeAreaView>
-  </Provider>
-);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <RootNavigation />
+      </SafeAreaView>
+    </Provider>
+  );
+};
 
 export default App;
