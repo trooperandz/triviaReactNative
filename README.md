a2f131c506ffd620d8973cad9b85796e9587d155
Matthew Holland <mtholland10@gmail.com>
2021-03-09 11:13:49 -0600

    Remove unused library mocks, add passing tests example

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 ### A little React Native mobile trivia app utilizing the Open Trivia DB
 
-##### Utilizes:
+#### Utilizes:
 
 - TypeScript
 - Styled Components
 - Redux Toolkit
 
+#### Demo:
+
 ![Example demo](src/assets/trivia-react-native-demo.gif)
+
+#### Tests:
+
+![Passing tests](src/assets/passing-tests.png)
--- __mocks__/libraryMocks.js
+++ __mocks__/libraryMocks.js
@@ -1,3 +1,2 @@
-// jest.mock('react-native-reanimated', () => {});
-// Remove the "nimated: `useNativeDriver` is not supported because the native animated module is missing" warning
+// Remove the "animated: `useNativeDriver` is not supported because the native animated module is missing" warning
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
